7c3cc1cb644aced7986b4ebbc148ccc6d1544d64
Jaime Ribeiro <littlejou@gmail.com>
2024-03-07 11:06:21 -0300

    Fix: Prohibits invalid insertion in Endpoints that use the Veiculo model

--- src/main/java/com/algaworks/algatransito/domain/model/Veiculo.java
+++ src/main/java/com/algaworks/algatransito/domain/model/Veiculo.java
@@ -1,5 +1,7 @@
 package com.algaworks.algatransito.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -21,12 +23,21 @@ public class Veiculo {
     //@JoinColumn(name = "proprietario_id") -não precisamos disso pq o jakarta persistence identifica automaticamente
     //Caso o nome da coluna não tivesse ****_id no nome, aí seria recomendado usar o join column especificando o nome
     private Proprietario proprietario;
+
     private String marca;
     private String modelo;
     private String placa;
+
+    //caso você import somente até JsonProperty você precisa colocar assim com "JsonProperty.Access.READ_ONLY"
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @Enumerated(EnumType.STRING)
     private StatusVeiculo status;
+
+    //caso o seu import seja JsonProperty.Access você poderá botar só Access.READ_ONLY que funcionará
+    @JsonProperty(access = Access.READ_ONLY)
     private LocalDateTime dataCadastro; //o jakarta configura mesmo que tenha _ no nome da coluna, ele identifica
+
+    @JsonProperty(access = Access.READ_ONLY)
     private LocalDateTime dataApreensao;
 
 }
