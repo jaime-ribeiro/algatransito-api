ad89e7c8136e711793b538e169c0e117bd3e0c5c
Jaime Ribeiro <littlejou@gmail.com>
2024-03-07 16:11:03 -0300

    fix: add RFC 7807 pattern in exceptions

--- src/main/java/com/algaworks/algatransito/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algatransito/api/exceptionhandler/ApiExceptionHandler.java
@@ -4,9 +4,11 @@ import com.algaworks.algatransito.domain.exception.NegocioException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+    //Usando o "extends ResponseEntityExceptionHandler" faz com que use a RFC 7807 (Problem Details for HTTP APIs)
     @ExceptionHandler(NegocioException.class)
     public ResponseEntity<String> capturar(NegocioException e){
         return ResponseEntity.badRequest().body(e.getMessage());
