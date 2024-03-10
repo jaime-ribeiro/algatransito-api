03c5dccad68067b6599a1a137fa0e98955063ea6
Jaime Ribeiro <13546394+jaime-ribeiro@users.noreply.github.com>
2024-03-10 19:34:36 -0300

    fix: Show invalid fields with error details

--- src/main/java/com/algaworks/algatransito/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algatransito/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.algaworks.algatransito.api.exceptionhandler;
 
 import com.algaworks.algatransito.domain.exception.NegocioException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,6 +14,8 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.net.URI;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
@@ -22,6 +26,13 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         ProblemDetail problemDetail = ProblemDetail.forStatus(status);
         problemDetail.setTitle("Um ou mais campos estão inválidos");
         problemDetail.setType(URI.create("http://algatransito.com/erros/campos-invalidos"));
+
+        Map<String, String> fields = ex.getBindingResult().getAllErrors()
+                        .stream()
+                        .collect(Collectors.toMap(objectError -> ((FieldError) objectError).getField(),
+                                DefaultMessageSourceResolvable::getDefaultMessage));
+                                //A mensagem pode aparecer em português de acordo com a linguagem do sistema operacional
+        problemDetail.setProperty("invlidFields", fields);
 
         return handleExceptionInternal(ex, problemDetail, headers,status ,request);
     }
